2f34633ae277db3acb01e99f9cc070840ccfe086
Alan Ho <alanho@gmail.com>
2012-05-19 00:29:46 +0800

    here we go... let's get this started!

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MotherSpeak::Application.config.secret_token = 'e5ebaa22b792ea820f1a4620f5e4ab24f21d3948e66c7db7e510153db16ee6b87ffcc15a1a050fda3ae606b5bb39b69438995ea12555a9d7c8ae78105b5ff611'
